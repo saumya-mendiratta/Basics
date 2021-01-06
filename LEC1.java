6f6014510ad37f40ad298aa1f4b7f08b7025b84c
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-01-07 00:39:26 +0530

    Create LEC1.java

---
+++ LEC1.java
@@ -0,0 +1,18 @@
+package LEC1;
+
+
+
+public class LEC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int p=100;
+	int r= 200;
+	int t=500;
+	 int si= p*r*t/100;
+	 System.out.println(" SI IS "+ si);
+	
+	}
+
+}
