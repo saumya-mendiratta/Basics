56655d087ea59bb6fbb8014928dd38789e67fe99
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-01-09 22:55:43 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,18 @@
+package LEC1;
+
+public class pattern {
+
+	public static void main(String[] args) {
+	int n= 5;
+	int row,col ;
+	for(row =1;row<=n;row++)
+	{
+		for(col=1;col<=row;col++)
+		{	System.out.print("*");
+		}
+	
+	System.out.println();
+	}
+	}
+
+}
