8b2f7cfe34c6773cfdf4bd9f2923a3bb3301a1f6
saumya-mendiratta <saya.mendiratta@gmail.com>
2021-01-11 06:27:13 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,35 @@
+
+package LEC1;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class pattern9 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int row = 1;
+		
+		int nsp =n-1 ;
+		int nst = 1;
+
+		while (row <=n) {
+			int csp = 1, cst = 1;
+			while (csp <= nsp) {
+				System.out.print(" ");
+				csp++;
+			}
+			while (cst <= nst) {
+				System.out.print("*");
+				cst = cst + 1;
+			}
+			System.out.println();
+
+			row++;
+			
+				nsp--;
+				nst+=2;
+			
+		}
+
+	}
+}
